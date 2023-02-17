e1a43cef20a0e1eb26a364728c63557f47f261ee
Marty <martynolan17@gmail.com>
2023-02-17 12:42:21 -0500

    added queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,62 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_PRODUCTS = gql`
+  query getProducts($console: ID) {
+    products(console: $console) {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      console {
+        _id
+      }
+    }
+  }
+`;
+
+export const QUERY_ALL_PRODUCTS = gql`
+  {
+    products {
+      _id
+      name
+      description
+      price
+      quantity
+      console {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_CONSOLES = gql`
+  {
+    consoles {
+      _id
+      name
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+      orders {
+        _id
+        purchaseDate
+        products {
+          _id
+          name
+          description
+          price
+          quantity
+          image
+        }
+      }
+    }
+  }
+`;
