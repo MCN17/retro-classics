b1e6db0c59614db38cfee92302a9d80c4e222680
Marty <martynolan17@gmail.com>
2023-03-30 09:06:21 -0400

    cart data saves to indexedDB

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Auth from "../../utils/auth";
 import "./style.css";
 import { useStoreContext } from "../../utils/GlobalState";
-import { TOGGLE_CART } from "../../utils/actions";
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 //import components
 import CartItem from '../CartItem';
@@ -10,6 +11,17 @@ import CartItem from '../CartItem';
 const Cart = () => {
 
     const [state, dispatch] = useStoreContext();
+
+    useEffect(() => {
+        async function getCart() {
+            const cart = await idbPromise("cart", "get");
+            dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart]});
+        };
+
+        if (!state.cart.length) {
+            getCart();
+        }
+    }, [state.cart.length, dispatch]);
 
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
